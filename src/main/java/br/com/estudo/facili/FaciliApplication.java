6d2a8cd47acd95888e21bf13fd1854c188bdd3f3
carlos daniel TMKT <carlosdaniel@turbomkt.com.br>
2021-10-03 21:10:48 -0300

    Inicializando projeto

---
+++ src/main/java/br/com/estudo/facili/FaciliApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudo.facili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaciliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaciliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/estudo/facili/FaciliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.facili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaciliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
