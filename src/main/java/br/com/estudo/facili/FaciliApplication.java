13672be965284b319e61d04fd83d59496ebab2bf
Eduardo da Silva Araújo <eduardodevfull@gmail.com>
2021-10-04 11:22:27 -0300

    Correção no projeto

--- src/main/java/br/com/estudo/facili/FaciliApplication.java
+++ src/main/java/br/com/estudo/facili/FaciliApplication.java
@@ -2,8 +2,10 @@ package br.com.estudo.facili;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class FaciliApplication {
 
 	public static void main(String[] args) {
