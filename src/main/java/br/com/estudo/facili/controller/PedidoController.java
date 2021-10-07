e6865bfea4c1b2678465a8b57de12e34ea1814a5
carlos daniel TMKT <carlosdaniel@turbomkt.com.br>
2021-10-07 19:09:17 -0300

    Adicionado listagem de pedidos

--- src/main/java/br/com/estudo/facili/controller/PedidoController.java
+++ src/main/java/br/com/estudo/facili/controller/PedidoController.java
@@ -3,13 +3,13 @@ package br.com.estudo.facili.controller;
 import br.com.estudo.facili.dto.PedidoRequest;
 import br.com.estudo.facili.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/pedidos")
+@CrossOrigin("*")
 public class PedidoController {
 
     @Autowired
@@ -19,5 +19,10 @@ public class PedidoController {
     public void criarPedido(@RequestBody PedidoRequest pedido) {
         System.out.println(pedido.getProduto());
         this.pedidoService.criaPedido(pedido);
+    }
+
+    @GetMapping
+    public List listarPedidos() {
+        return this.pedidoService.listaTodosPedidos();
     }
 }
